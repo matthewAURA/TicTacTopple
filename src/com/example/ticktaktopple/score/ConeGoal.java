85ea29d959a8cb68a516e3d77ed7d515f47cb453
Matthew Dyer <matthew.dyer@aura.org.nz>
2014-03-27 18:18:21 +1300

    Revert "Fixed #6. Cone Goal Scoring now correct"
    This reverts commit d99c46a595ccfd0ba6d3203d9ed05a86d6af9af8.

--- src/com/example/ticktaktopple/score/ConeGoal.java
+++ src/com/example/ticktaktopple/score/ConeGoal.java
@@ -2,21 +2,12 @@ package com.example.ticktaktopple.score;
 
 public class ConeGoal extends Goal {
 
-	private Colour lastColour;
-	
-	public void addObjectsCount(Colour colour,int objects){
-		super.addObjectsCount(colour, objects);
-		this.lastColour = colour;
-	}
 	
 	@Override
 	public int getPoints(Colour goal) {
 		return 3*this.getObjects(goal)+this.getBonus(goal);
 	}
 	
-	public Colour hasBonus(){
-		return this.lastColour;
-	}
 
 
 }
